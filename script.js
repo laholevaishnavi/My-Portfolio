3e98f5716bf2c9542bfdca8cc699d0d543821d79
laholevaishnavi <laholevaishnavi11@gmail.com>
2024-06-26 14:03:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// const fun_color = () =>
+// {let btn = document.querySelector("button");
+// btn.style.background= "blue";
+
+// }
+// const download= () =>{
+//  window.print(); print //the web page or document
+// }
