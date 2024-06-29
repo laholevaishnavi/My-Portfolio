27699157cbadf2afdf567920b44579d47750badc
laholevaishnavi <laholevaishnavi11@gmail.com>
2024-06-29 17:51:29 +0530

    Hamburger menu added

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// const fun_color = () =>
-// {let btn = document.querySelector("button");
-// btn.style.background= "blue";
-
-// }
-// const download= () =>{
-//  window.print(); print //the web page or document
-// }
+function menuOnclick(){
+  document.querySelector('.navbar').style.display = "block";
+  document.querySelector('.ri-menu-line').style.display = "none" ;
+  document.querySelector('.ri-close-line').style.display = "inline" ;
+}
+function closeOnclick(){
+  document.querySelector('.navbar').style.display = "none";
+  document.querySelector('.ri-menu-line').style.display = "inline-block" ;
+  document.querySelector('.ri-close-line').style.display = "none" ;
+}
